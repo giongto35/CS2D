b89a463b591e4dc83041503b04403dfb97f32b1f
giongto35 <giongto35@yahoo.com>
2015-07-21 11:34:22 +0700

    fix

--- client/client.js
+++ client/client.js
@@ -7,8 +7,8 @@ var screenWidth = window.innerWidth;
 var screenHeight = window.innerHeight;
 var xoffset = -constant.GAME_WIDTH;
 var yoffset = -constant.GAME_HEIGHT;
-// var socket = new WebSocket('ws://127.0.0.1');
-// socket.binaryType = 'arraybuffer';
+var socket = new WebSocket('ws://127.0.0.1:3000');
+socket.binaryType = 'arraybuffer';
 
 //make register function for command
 
